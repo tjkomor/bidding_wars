a50807eae750c67d1c526ba41757945b9a1294b9
rossedfort <rossedfort@gmail.com>
2015-11-09 17:00:25 -0700

    fixed hound complaints

--- spec/features/visitor_can_view_items_spec.rb
+++ spec/features/visitor_can_view_items_spec.rb
@@ -1,31 +1,24 @@
-# Background: I have several items and each of them has a title, description, price, and image
-#       I can see all existing items
-
 require "rails_helper"
 
 feature "View items" do
+  let!(:item_one) {Item.create(name: "Canon 7d",
+                                description: "The best camera ever (NOT)",
+                                price: 15)}
 
-    let!(:item_one) {Item.create(name: "Canon 7d",
-                                 description: "The best camera ever (NOT)",
-                                 price: 15)}
-
-    let!(:item_two) {Item.create(name: "Sony a7r",
-                                 description: "Robbies camera",
-                                 price: 12)}
+  let!(:item_two) {Item.create(name: "Sony a7r",
+                                description: "Robbies camera",
+                                price: 12)}
 
   scenario "visitor can view items" do
     visit "/items"
-
     within("#heading") do
       expect(page).to have_content("Items")
     end
-
     within("#item_#{item_one.id}") do
       expect(page).to have_content(item_one.name)
       expect(page).to have_content(item_one.description)
       expect(page).to have_content(item_one.price)
     end
-    
     within("#item_#{item_two.id}") do
       expect(page).to have_content(item_two.name)
       expect(page).to have_content(item_two.description)
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -8,21 +8,18 @@ RSpec.describe Item, type: :model do
 
     expect(item).to be_valid
   end
-
   it "is invalid without a name" do
     item = Item.create(description: "The best camera ever (NOT)",
                        price: 15)
 
     expect(item).to_not be_valid
   end
-
   it "is invalid without a description" do
     item = Item.create(name: "Canon 7d",
                        price: 15)
 
     expect(item).to_not be_valid
   end
-
   it "is invalid without a price" do
     item = Item.create(name: "Canon 7d",
                        description: "The best camera ever (NOT)")
