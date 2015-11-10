d504b8115e0db17dbf66a5068ce4754b193b61d9
rossedfort <rossedfort@gmail.com>
2015-11-09 17:45:40 -0700

    changed 'create' to 'new' in model validation tests

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,26 +2,26 @@ require "rails_helper"
 
 RSpec.describe Item, type: :model do
   it "is valid with name, description, price" do
-    item = Item.create(name: "Canon 7d",
+    item = Item.new(name: "Canon 7d",
                        description: "The best camera ever (NOT)",
                        price: 15)
 
     expect(item).to be_valid
   end
   it "is invalid without a name" do
-    item = Item.create(description: "The best camera ever (NOT)",
+    item = Item.new(description: "The best camera ever (NOT)",
                        price: 15)
 
     expect(item).to_not be_valid
   end
   it "is invalid without a description" do
-    item = Item.create(name: "Canon 7d",
+    item = Item.new(name: "Canon 7d",
                        price: 15)
 
     expect(item).to_not be_valid
   end
   it "is invalid without a price" do
-    item = Item.create(name: "Canon 7d",
+    item = Item.new(name: "Canon 7d",
                        description: "The best camera ever (NOT)")
 
     expect(item).to_not be_valid
