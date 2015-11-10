b53bcc3731487dbc5aac421561c4df49aabf5649
Tyler Komoroske <tjkomor@clemson.edu>
2015-11-10 10:26:49 -0700

    add items to cart functionality, half working as of now

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,6 +1,11 @@
 class CartController < ApplicationController
 
   def index
+    @cart_items = session[:cart]
+    item_ids = session[:cart].keys.map { |key| key.to_i }
+    binding.pry
+    @items = Item.where(item_id: item_ids)
+
   end
 
 end
--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,9 +1,15 @@
 class CartItemsController < ApplicationController
 
   def create
+    item = Item.find(params[:item_id])
+    @cart.add_item(item.id)
+    # binding.pry
+    session[:cart] = @cart.items
+    flash[:notice] = "#{item.name} Added to Cart!"
     render :index
   end
 
   def index
+
   end
 end
