e6bb7cb78c7eac059dea49971fa7adc35ba2b35b
rossedfort <rossedfort@gmail.com>
2015-11-14 12:15:01 -0700

    add some buttons and content to suer show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
   validates :username, presence: true, uniqueness: true
   validates :password, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email_address, presence: true, uniqueness: true
+  validates :phone_number, presence: true, uniqueness: true
   has_many :rentals
   has_secure_password
 
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -40,7 +40,7 @@ RSpec.configure do |config|
   end
 
   def create_user
-    let!(:user_one) { User.create(username: 'rossedfort', password: 'password') }
+    let!(:user_one) { User.create(username: 'rossedfort', password: 'password', first_name: 'Ross', last_name: 'Edfort', email_address: 'rossedfort@yahoo.com', phone_number: '908-698-9024') }
   end
 
   def create_rental
