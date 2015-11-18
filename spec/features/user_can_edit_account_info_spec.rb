0b96dddd48e3a2d96a2cbcc8f59de26af66a3be3
rossedfort <rossedfort@gmail.com>
2015-11-18 09:32:41 -0700

    implement more sad path tests for:
    -admin item creation
    -admin item edit
    -visitor account creation
    -visitor account update

--- spec/features/user_can_edit_account_info_spec.rb
+++ spec/features/user_can_edit_account_info_spec.rb
@@ -27,5 +27,136 @@ RSpec.describe "User", type: :feature do
       expect(page).to have_content("User Updated!")
       expect(page).to have_content("Hello New First Name!")
     end
+    it "gets an error when password is left out" do
+      create_rental_items
+      login_user
+      visit dashboard_path
+
+      click_link_or_button "Edit Info"
+
+      expect(current_path).to eq(edit_user_path(user_one))
+
+      fill_in 'Username', with: 'NewUsername'
+      fill_in 'First name', with: 'New First Name'
+      fill_in 'Last name', with: 'New Last Name'
+      fill_in 'Email address', with: 'new_email_address@fake.com'
+      fill_in 'Phone number', with: '101-101-1001'
+      click_button 'Update'
+
+      expect(page).to have_content("Password cannot be blank")
+      expect(page).to_not have_content("Hello New First Name!")
+    end
+    it "gets an error when username is left out" do
+      create_rental_items
+      login_user
+      visit dashboard_path
+
+      click_link_or_button "Edit Info"
+
+      expect(current_path).to eq(edit_user_path(user_one))
+
+      fill_in 'Username', with: ''
+      fill_in 'Password', with: 'newpassword'
+      fill_in 'First name', with: 'New First Name'
+      fill_in 'Last name', with: 'New Last Name'
+      fill_in 'Email address', with: 'new_email_address@fake.com'
+      fill_in 'Phone number', with: '101-101-1001'
+      click_button 'Update'
+
+      expect(page).to have_content("Username cannot be blank")
+      expect(page).to_not have_content("Hello New First Name!")
+    end
+    it "gets an error when first name is left out" do
+      create_rental_items
+      login_user
+      visit dashboard_path
+
+      click_link_or_button "Edit Info"
+
+      expect(current_path).to eq(edit_user_path(user_one))
+
+      fill_in 'Username', with: 'NewUsername'
+      fill_in 'Password', with: 'newpassword'
+      fill_in 'First name', with: ''
+      fill_in 'Last name', with: 'New Last Name'
+      fill_in 'Email address', with: 'new_email_address@fake.com'
+      fill_in 'Phone number', with: '101-101-1001'
+      click_button 'Update'
+
+      expect(page).to have_content("First name cannot be blank")
+      expect(page).to_not have_content("Hello New First Name!")
+    end
+    it "gets an error when last name is left out" do
+      create_rental_items
+      login_user
+      visit dashboard_path
+
+      click_link_or_button "Edit Info"
+
+      expect(current_path).to eq(edit_user_path(user_one))
+
+      fill_in 'Username', with: 'NewUsername'
+      fill_in 'Password', with: 'newpassword'
+      fill_in 'First name', with: 'New First Name'
+      fill_in 'Last name', with: ''
+      fill_in 'Email address', with: 'new_email_address@fake.com'
+      fill_in 'Phone number', with: '101-101-1001'
+      click_button 'Update'
+
+      expect(page).to have_content("Last name cannot be blank")
+      expect(page).to_not have_content("Hello New First Name!")
+    end
+    it "gets an error when email address is left out" do
+      create_rental_items
+      login_user
+      visit dashboard_path
+
+      click_link_or_button "Edit Info"
+
+      expect(current_path).to eq(edit_user_path(user_one))
+
+      fill_in 'Username', with: 'NewUsername'
+      fill_in 'Password', with: 'newpassword'
+      fill_in 'First name', with: 'New First Name'
+      fill_in 'Last name', with: 'New Last Name'
+      fill_in 'Email address', with: ''
+      fill_in 'Phone number', with: '101-101-1001'
+      click_button 'Update'
+
+      expect(page).to have_content("Email address cannot be blank")
+      expect(page).to_not have_content("Hello New First Name!")
+    end
+    it "gets an error when email address is left out" do
+      create_rental_items
+      login_user
+      visit dashboard_path
+
+      click_link_or_button "Edit Info"
+
+      expect(current_path).to eq(edit_user_path(user_one))
+
+      fill_in 'Username', with: 'NewUsername'
+      fill_in 'Password', with: 'newpassword'
+      fill_in 'First name', with: 'New First Name'
+      fill_in 'Last name', with: 'New Last Name'
+      fill_in 'Email address', with: 'example@example.com'
+      fill_in 'Phone number', with: ''
+      click_button 'Update'
+
+      expect(page).to have_content("Phone number cannot be blank")
+      expect(page).to_not have_content("Hello New First Name!")
+    end
+    it "can delete account", js: true do
+      create_rental_items
+      login_user
+
+      click_link "Delete Account"
+      sleep(1)
+      click_on "Yes"
+
+      expect(page).to have_content("Login")
+      expect(page).to have_content("Your account has been deactivated")
+      expect(current_path).to eq(root_path)
+    end
   end
 end
