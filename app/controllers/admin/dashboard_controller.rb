479864bfdac669665341b90a75dd5735cc3bfcee
Robbie Lane <lane.robbie@gmail.com>
2015-11-19 08:54:01 -0700

    fix admin namespacing

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,4 @@
-class Admin::AdminController < Admin::BaseController
+class Admin::DashboardController < Admin::BaseController
   def show
     @admin_dash = true
     @rentals = Rental.all
--- config/routes.rb
+++ config/routes.rb
@@ -12,16 +12,11 @@ Rails.application.routes.draw do
     resources :addresses, only: [:edit, :update]
   end
   namespace :admin do
-    get '/dashboard', to: 'admin#show'
+    get '/dashboard', to: 'dashboard#show'
     resources :categories, only: [:new, :create]
     resources :rentals, only: [:show, :update]
     resources :items, only: [:new, :create, :index, :edit, :update]
     resources :brands, only: [:new, :create]
-    get '/dashboard/completed', to: 'admin#completed'
-    get '/dashboard/ordered', to: 'admin#ordered'
-    get '/dashboard/out', to: 'admin#out'
-    get '/dashboard/cancelled', to: 'admin#cancelled'
-    get '/dashboard/paid', to: 'admin#paid'
   end
   get '/dashboard', to: 'users#show'
   get '/cart', to: 'cart#index'
