b59568e986e5f7183d4b1cec23cbfd5dcfb5dfd6
Amber Crawford <ammcrawford@gmail.com>
2015-12-02 13:31:22 -0700

    Update test to complete user bid experience

--- spec/features/guest_must_be_logged_in_to_make_bid_spec.rb
+++ spec/features/guest_must_be_logged_in_to_make_bid_spec.rb
@@ -4,18 +4,37 @@ RSpec.describe 'Guest', type: :feature do
   create_store
   create_category
   create_items
+  create_user
   feature 'Bidding' do
     it 'must be logged in' do
       visit root_path
 
       click_link "Video Games"
-      
+
+      within("#item_#{item_one.id}") do
+        click_button "See Auction Details"
+      end
+
+      click_button "Bid!"
+
+      expect(current_path).to eq(login_path)
+      expect(page).to have_content("Login")
+
+      fill_in 'Username', with: 'rossedfort'
+      fill_in 'Password', with: 'password'
+      click_button "Login"
+
+      expect(current_path).to eq(item_path(item_one.id))
+
       within("#item_#{item_one.id}") do
         click_button "Bid!"
       end
 
-      expect(current_path).to eq(login_path)
-      expect(page).to have_content("Login")
+      expect(page).to have_content("Successfully Bid on Item")
+
+      visit '/dashboard'
+
+      expect(page).to have_content("#{item_one.name}")
     end
   end
 end
