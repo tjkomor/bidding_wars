773888d41d868da812a80010503335c3d3ad169d
Ryan Johnson <rjohnson4444@live.com>
2015-12-03 19:27:14 -0700

    add user edit functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ActiveRecord::Base
   has_secure_password
+  validates :password, presence: true
+  validates :email_address, presence: true
+  validates :phone_number, presence: true
+  validates :username, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  # validates :username, presence: true
+  # validates :username, presence: true
 
   has_many :bid_histories
   has_many :items, through: :bid_histories
