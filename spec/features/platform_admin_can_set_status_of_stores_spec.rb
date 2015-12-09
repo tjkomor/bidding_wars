003afc31aed473a389b3ad9eef79f410852fa27b
Tyler Komoroske <tjkomor@clemson.edu>
2015-12-09 12:43:30 -0700

    store admin can deactivate account

--- spec/features/platform_admin_can_set_status_of_stores_spec.rb
+++ spec/features/platform_admin_can_set_status_of_stores_spec.rb
@@ -34,5 +34,43 @@ RSpec.describe "Platform Admin", type: :feature do
 
       expect(page).to have_content("Active")
     end
+
+    it 'can deactivate store' do
+      platform_admin = User.create(username: 'platform_admin', password: 'platform_admin', first_name: 'John', last_name: 'Smith', email_address: 'johnsmith@gmail.com', phone_number: '555-234-5678')
+      platform_admin.roles.clear << Role.where(name: 'platform_admin').first
+      admin_one.roles.clear << Role.where(name: 'store_admin').first
+      new_store = admin_one.stores.create(name: "Tyler")
+
+      visit login_path
+
+      fill_in "Username", with: 'platform_admin'
+      fill_in "Password", with: 'platform_admin'
+
+      click_button "Login"
+
+      expect(page).to have_content("Platform Admin Dashboard")
+      expect(page).to_not have_content("Login")
+
+      click_button "View Stores"
+
+      within("#store-#{new_store.id}") do
+        expect(page).to have_content("Pending")
+        click_button "Activate"
+      end
+
+      visit platform_admin_stores_path
+
+      within("#store-#{new_store.id}") do
+        expect(page).to have_content("Active")
+        click_button "Deactivate"
+      end
+
+      visit platform_admin_stores_path
+
+      within("#store-#{new_store.id}") do
+        expect(page).to have_content("Deactivated")
+        expect(page).to have_button("Activate")
+      end
+    end
   end
 end
