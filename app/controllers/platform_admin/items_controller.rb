4fe28824e6c5b3de4ba963bdfcbb465e5b8c1c8b
Amber Crawford <ammcrawford@gmail.com>
2015-12-09 22:29:26 -0700

    Platform admin can edit an item

--- app/controllers/platform_admin/items_controller.rb
+++ app/controllers/platform_admin/items_controller.rb
@@ -13,14 +13,35 @@ class PlatformAdmin::ItemsController < PlatformAdmin::BaseController
     if @item.save
         flash[:notice] = "Item Created!"
 
-        redirect_to platform_admin_store_path(id: Store.find(@item.store_id).slug)
+        redirect_to platform_admin_store_path(id: find_store_slug)
     else
       flash.now[:error] = @item.errors.full_messages.first.gsub("can't", "cannot")
       render :new
     end
   end
 
+  def edit
+    @item = Item.find(params[:format])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      @item.set_permissions
+      flash[:notice] = "Item Updated!"
+      redirect_to platform_admin_items_path(format: @item.store.slug)
+    else
+      flash[:error] = @item.errors.full_messages.first.gsub("can't", "cannot")
+      redirect_to platform_admin_items_path
+    end
+
+  end
+
 private
+
+  def find_store_slug
+    Store.find(@item.store_id).slug
+  end
 
   def find_store
     Store.find_by(slug: params[:format])
