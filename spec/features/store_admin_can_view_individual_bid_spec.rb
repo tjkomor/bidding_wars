b3b8cc6d3b4916571a3e9f7f1a17f20a30e9e3bf
Ryan Johnson <rjohnson4444@live.com>
2015-12-09 19:28:40 -0700

    test coverage at 81%

--- spec/features/store_admin_can_view_individual_bid_spec.rb
+++ spec/features/store_admin_can_view_individual_bid_spec.rb
@@ -2,32 +2,30 @@ require 'rails_helper'
 RSpec.describe 'store admin', type: :feature do
   create_category
   create_store
+  create_store_admin
+  create_roles
   create_items
   create_user
 
   feature 'can view' do
-    xit 'individual order info' do
+    it 'individual order info' do
+      admin_one.roles << Role.where(name: 'store_admin')
+      admin_one.stores.create(name: 'Amber')
+      admin_one_store = admin_one.stores.first
+      user_one.orders.create(store_id: admin_one_store.id, amount: 30, item_id: item_one.id)
+      admin_one.stores.first.items << item_one
+
       visit login_path
 
       fill_in "Username", with: 'admin'
       fill_in "Password", with: 'admin'
 
-      click_button "View Customers"
+      click_button "Login"
 
-      click_link "Ross Edfort"
+      click_link "View Customers"
 
-      expect(page).to have_content('Order History')
-
-      click_link rental_one.id
-      within("#order_item_row_#{order_one.items.first.id}") do
-        expect(page).to have_content('Casino')
-        expect(page).to have_content("Winning bid")
-      end
-      expect(page).to have_content("Ross Edfort")
-      expect(page).to have_content('$30')
-      expect(page).to have_content('Completed')
-      # expect(page).to have_content("1510 Blake Street Denver CO, 80202")
-      expect(page).to have_content("08:29AM - 11/11/2015")
+      expect(page).to have_content("Ross")
+      expect(page).to have_content("#{item_one.name}")
     end
   end
 end
